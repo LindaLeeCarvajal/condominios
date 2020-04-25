7a672c201f7778a3293807a1d9575aa45e2ccf20
Linda Carvajal <carvajal00lee@gmail.com>
2020-04-25 00:27:00 -0400

    primer commit prueba

--- model/Conexion.php
+++ model/Conexion.php
@@ -1,6 +1,6 @@
 <?php
 
- $conexion = new mysqli("localhost", "root", "", "bdcondomin");
+ $conexion = new mysqli("localhost", "root", "", "bdcondominios");
 
 	if (mysqli_connect_errno()) {
 	    printf("Connect failed: %s\n", mysqli_connect_error());
